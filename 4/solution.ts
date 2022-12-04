c39848b84d6873bea3dc69e5e89c766ed9b487ba
Kacper Gumieniuk <kacpergumieniuk@gmail.com>
2022-12-04 12:34:43 +0100

    Day 4 part 2

--- 4/solution.ts
+++ 4/solution.ts
@@ -19,4 +19,40 @@ for (let x of splited) {
   checkIfFullyContains(x[0], x[1])
 }
 
-console.log(fullyContains)
+console.log(`Result of part one is ${fullyContains}`)
+
+//PART2
+
+let isOverlaping = 0
+
+const checkIfOverlaps = (range1: string, range2: string) => {
+  const range1splited = range1.split('-')
+  const range2splited = range2.split('-')
+  const range1Arr: number[] = []
+  const range2Arr: number[] = []
+  for (
+    let x = parseInt(range1splited[0]);
+    x <= parseInt(range1splited[1]);
+    x++
+  ) {
+    range1Arr.push(x)
+  }
+  for (
+    let x = parseInt(range2splited[0]);
+    x <= parseInt(range2splited[1]);
+    x++
+  ) {
+    range2Arr.push(x)
+  }
+  const contains = range1Arr.some((element) => {
+    return range2Arr.includes(element)
+  })
+
+  contains && isOverlaping++
+}
+
+for (let x of splited) {
+  checkIfOverlaps(x[0], x[1])
+}
+
+console.log(`Result of part two is ${isOverlaping}`)
