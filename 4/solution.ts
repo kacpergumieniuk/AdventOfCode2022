b5c024e53495783e5a6a07a5645f95c16d70551f
Kacper Gumieniuk <kacpergumieniuk@gmail.com>
2022-12-04 11:11:26 +0100

    Part 1 day 4

---
+++ 4/solution.ts
@@ -0,0 +1,22 @@
+const input = await Deno.readTextFile('./input.txt')
+const splited = input.split(/\r?\n/).map((str) => str.split(','))
+
+let fullyContains = 0
+
+const checkIfFullyContains = (range1: string, range2: string) => {
+  const range1splited = range1.split('-')
+  const range2splited = range2.split('-')
+  const condition1 = parseInt(range1splited[0]) <= parseInt(range2splited[0])
+  const condition2 = parseInt(range1splited[1]) >= parseInt(range2splited[1])
+  const condition3 = parseInt(range2splited[1]) >= parseInt(range1splited[1])
+  const condition4 = parseInt(range2splited[0]) <= parseInt(range1splited[0])
+  if ((condition1 && condition2) || (condition3 && condition4)) {
+    fullyContains++
+  }
+}
+
+for (let x of splited) {
+  checkIfFullyContains(x[0], x[1])
+}
+
+console.log(fullyContains)
